dc432a4a1fee800c212bf8d0b05db344efa18f3d
Justin Herrick <justin@justinherrick.com>
2015-10-26 15:09:25 -0500

    Fixing broken code on heroku by URL escaping the text input.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
-class HomeController < ApplicationController
-  def index
+require 'uri'
+
+class HomeController < ApplicationController #The name of this controller does not matter
+  def index # This is the page that displays the search field
   end
 
   def search
     weather_url = 'http://api.openweathermap.org/data/2.5/weather' #Remote weather API that returns JSON (Reference: http://openweathermap.org/current)
     request_url = 'http://rpc.geocoder.us/service/json?address=' #Remote 'geocoder' API that returns JSON (Reference: http://geocoder.us/help/)
     address  = params[:query]
-    response = HTTParty.get(request_url + address)
+    puts address.inspect
+    response = HTTParty.get(request_url + URI.escape(address))
     hash_response = JSON.parse(response)
     puts hash_response.inspect   #<< Having to puts out the response to the log helps figure out what keys I need to look at.
     lat  = hash_response[0]["lat"]
